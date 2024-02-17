e319de14e7270136fe84bc6df82719607485c64d
Garret Carmouche <carmouchegarret@gmail.com>
2024-02-17 16:50:23 -0600

    Remove some logs

--- frontend/react-frontend/src/FileList.js
+++ frontend/react-frontend/src/FileList.js
@@ -3,10 +3,7 @@ import FileItem from "./FileItem"
 
 function FileList(files){
 	console.log("File list page got files")
-	console.log(typeof files)
 	console.log(files)
-	console.log(files.length)
-	console.log(files[0])
 
 	return (
 		<div>
