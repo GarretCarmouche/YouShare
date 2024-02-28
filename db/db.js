5f920d49e0d5a2f98f35f18e88ee26b62c41f4c9
Garret Carmouche <carmouchegarret@gmail.com>
2024-02-28 11:37:32 -0600

    Hide secrets

--- db/db.js
+++ db/db.js
@@ -1,10 +1,13 @@
 const { Pool } = require("pg")
+const fs = require("fs")
+
+const dbpass = fs.readFileSync("/run/secrets/db-password", "utf8")
 
 const pool = new Pool({
 	host: "db",
 	port: 5432,
 	user: "postgres",
-	password: "admin",
+	password: dbpass,
 	database: "authentication"
 })
 
