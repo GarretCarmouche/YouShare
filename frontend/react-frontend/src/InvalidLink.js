dd5f00be4a0a3a4cfe16dc94777dee3753c320a9
Garret Carmouche <carmouchegarret@gmail.com>
2024-03-01 11:22:38 -0600

    Invalid download key

---
+++ frontend/react-frontend/src/InvalidLink.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function InvalidLink(){
+	return (
+		<div>
+			<h1>
+				Invalid download or upload link
+			</h1>
+			<div>
+				The link you are trying to use is invalid. Please check the link and try again, or request a new one from the sender.
+			</div>
+		</div>
+	)
+}
+
+export default InvalidLink
