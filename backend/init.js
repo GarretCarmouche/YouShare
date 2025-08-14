9d335c7709214f4a514c13dd99a798af512b5559
Garret Carmouche <carmouchegarret@gmail.com>
2025-08-14 10:17:41 -0500

    Update docker CI/CD

--- backend/init.js
+++ backend/init.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const pool = require("../db/db")
+const pool = require("./db")
 const fs = require("fs")
 const multer = require("multer")
 const cors = require("cors")
